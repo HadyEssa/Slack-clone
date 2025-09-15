a1d2ced8b9e73d6159bb5d059af15a487081286c
Hady Essa <hadyessa585@gmail.com>
2025-09-16 02:24:17 +0300

    fix structre

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,8 +1,7 @@
-import "dotenv/config"
+import "dotenv/config";
 
 export const ENV = {
     PORT : process.env.PORT || 5000,
-    MONGO_URI : process.env.MONGO_URI ,
-    NODE_ENV : process.env.NODE_ENV || "development",
-
+    MONGO_URI: process.env.MONGO_URI,
+    NODE_ENV:process.env.NODE_ENV,
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 
+
 const app = express();
 
+
+
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.send("Hello World 123");
 })
 
-app.listen(ENV.PORT, ()=>{
-    console.log("Server is running on port " , ENV.PORT);
-})
+app.listen(ENV.PORT,()=>console.log("server is running", ENV.PORT))
