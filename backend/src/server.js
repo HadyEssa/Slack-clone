166d28fb726971fdea1767b1e1577a95fe3785c8
Hady Essa <hadyessa585@gmail.com>
2025-09-19 10:31:59 +0300

    Refactor server.js for code consistency and formatting improvements

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,9 @@
-import express from "express";
+import express from 'express';
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
-import {clerkMiddleware} from "@clerk/express";
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { functions, inngest } from "./config/inngest.js";
-import express from "express";
-
 
 const app = express();
 
@@ -14,23 +12,22 @@ app.use(express.json());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
+app.get("/", (req, res) => {
+  res.send("Hello World 123");
+});
 
-app.get("/",(req,res)=>{
-    res.send("Hello World 123");
-})
-
-const startServer = async ()=>{
-    try{
-        await connectDB();
-    if(ENV.NODE_ENV !== "production"){
-        app.listen(ENV.PORT,()=>{
-            console.log("server is running", ENV.PORT);
-        })
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("server is running", ENV.PORT);
+      });
     }
-    }catch(error){
-        console.error("Error starting server:", error);
-    }
-}
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+};
 startServer();
 
 export default app;
